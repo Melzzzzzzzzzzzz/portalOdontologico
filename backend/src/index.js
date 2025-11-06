db7f2514455d968c67bc7fb876c25f82c3b37181
Melzzzzzzzzzzzz <163297189+Melzzzzzzzzzzzz@users.noreply.github.com>
2025-11-06 12:01:36 -0300

    construção do site

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import router from "./routes/index.js"
-
-
-const app = express()
-
-app.use('/portaOdontologico', router)
--- backend/src/routes/routerListagemUsuario.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express"
-import controllerListagemUsuario from "../controller/controllerListagemUsuario.js"
-
-const router = Router()
-
-router.get("/listar-usuario", controllerListagemUsuario)
